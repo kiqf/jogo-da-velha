56bd0f3481ccf90738bc40ed0ce728a9bfc0ad2a
kiqf <139188372+kiqf@users.noreply.github.com>
2024-01-31 09:29:31 -0300

    feat: Initial Script DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello")
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = documentquerySelectorAll("#button-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
